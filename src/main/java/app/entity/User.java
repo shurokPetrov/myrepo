e1a09af8aa8e36b815c2ca9604a956ff64e88d29
olpetrov <alexandrpetrov.mk@gmail.com>
2022-06-14 18:06:09 +0300

    update pom and entity

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -6,6 +6,8 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @NoArgsConstructor
@@ -14,6 +16,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     @NonNull
     private String name;
